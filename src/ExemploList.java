33f326ee8e73652eb58cb2d04a563fa6d94ec812
Bruna <psbru2012@gmail.com>
2022-08-01 18:34:47 -0300

    add gitignore

--- src/ExemploList.java
+++ src/ExemploList.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
-    import java.util.Collections;
-    import java.util.Iterator;
-    import java.util.List;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 public class ExemploList {
 
 private static String[] args;
@@ -12,23 +12,23 @@ private static String[] args;
         //Formas criar listas usando Collections Java
 
         /*
-        1° List notas = new ArrayList<>(); //antes do Java 5
+        1? List notas = new ArrayList<>(); //antes do Java 5
 
-        2° List<Double> notas = new ArrayList<>() //Generics(jdk 5) - Diamont Operator // Diam.Oper. usa só o sina<> sem o "Double"
+        2? List<Double> notas = new ArrayList<>() //Generics(jdk 5) - Diamont Operator // Diam.Oper. usa s? o sina<> sem o "Double"
 
-        3° ArrayList<Double>notas = new Array List<>(); //Não recomendado
+        3? ArrayList<Double>notas = new Array List<>(); //N?o recomendado
 
-        4° List<Double> notas = new ArrayList<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
+        4? List<Double> notas = new ArrayList<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
 
-        5° List<Double> notas = Array.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
+        5? List<Double> notas = Array.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
         notas.add(10d);
         System.out.println(notas);
-        A 5° Forma é Limitada pois não há como modificar o array
+        A 5? Forma ? Limitada pois n?o h? como modificar o array
 
-        6° List<Double> notas = Listof(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
+        6? List<Double> notas = Listof(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
         notas.add(1d);
         System.out.println(notas);
-        A 6° Forma é Limitada pois não há como modificar o array
+        A 6? Forma ? Limitada pois n?o h? como modificar o array
         */
         System.out.print("Criando uma lista e adicionando as sete notas: ");
         List<Double> notas = new ArrayList<>(); // Melhor forma
@@ -58,18 +58,46 @@ private static String[] args;
         System.out.println("A menor nota é: " + Collections.max(notas));
 
         //Soma de dos valores dentro da lista
-        // É preciso chamar o método
+        // ? preciso chamar o método
+        double soma = 0;
         Iterator<Double> iterator = notas.iterator();
         while(iterator.hasNext()){
             Double next = iterator.next();
             soma += next;
         }
-        //Iterator há proximo? se true soma (significado do hasNext)
+        //Iterator h? proximo? se true soma (significado do hasNext)
         System.out.println("A soma dos valores é: " + soma);
 
+        /*Para exibir a m?dia ? preciso saber a quantidade de elementos da lista e
+        pra isso utiliza-se o método size
+         */
+        System.out.println("A média das notas é: " + (soma/notas.size()));
 
+        /* Para remo??o de um dos valores usa-se o método remove que pode ter como parametro
+        tanto a posi??o quanto o objeto
+         */
+        System.out.println("Removendo a nota 9.2: ");
+        //OBS: Não esquecer de ultilizar o "d" pois a falta do "d" remover? a posi??o
+        notas.remove(9.2d);
+        System.out.println(notas);
 
+        /* Para remover as notas menores que 7 ultiliza-se i Iterator
+        Pode-se criar uma v?ri?vel
+         */
+        Iterator<Double> iterator1 = notas.iterator();
 
+        while(iterator1.hasNext()) {
+            Double next = iterator1.next();
+            if(next< 7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        //Para apagar toda a lista use o método clear
+        //notas.clear();
+
+        /*Para conferir se a lista está vazia use o método
+        notas.isEmpty();
+         */
     }
 }
 
